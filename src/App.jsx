5f09685c328cb3ab27bc34ef85b2525a13549fe7
Falouhiayoub <falouhiayoub9@gmail.com>
2025-12-30 23:57:17 +0100

    add the protected routing for the admin Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import AdminDashboard from './pages/AdminDashboard'
 import Events from './pages/Events'
+import AdminLayout from './components/layout/AdminLayout'
 import Cart from './pages/Cart'
 import Checkout from './pages/Checkout'
+import AdminLogin from './pages/AdminLogin'
+import AdminOrders from './pages/AdminOrders'
 import './index.css'
 
+
+// Protected Route 
+const PrivateRoute = ({children}) => {
+  const user = JSON.parse(localStorage.getItem('user'))
+  return user && user.username === 'admin' ? children : <Navigate to= '/admin'/>
+}
 const App = () => {
   return (
     
         <Routes>
-          <Route path='/admin/dashboard' element = {<AdminDashboard/>}/>
+          {/* user Routes */}
           <Route path='/events' element= {<Events/>}/>
           <Route path= '/cart' element = {<Cart/>}/>
           <Route path="/checkout" element={<Checkout />} />
 
+          {/* admin Routes */}
+          <Route path='/admin' element = {<AdminLogin/>}/>
+          <Route path='/admin/dashboard' element = {
+            <PrivateRoute>
+              <AdminLayout>
+                <AdminDashboard/>
+              </AdminLayout>
+            </PrivateRoute>
+          }/>
+
+          <Route path='/admin/orders' element = {
+            <PrivateRoute>
+              <AdminLayout>
+                <AdminOrders/>
+              </AdminLayout>
+            </PrivateRoute>
+          }/>
         </Routes>
   )
 }
